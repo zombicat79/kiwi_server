7ef8d216f1c5ebc2cc55bd84d5cfead5b0440d0f
David Castejon <ombengue@hotmail.com>
2021-05-24 10:02:14 +0200

    Connect back end to the client side

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:3000']
+}));
+
+app.get('/:id', (req, res, next) => {
+    console.log(req.params);
+    res.send("hello");
+})
+
+app.listen(5000, () => console.log("Listening on port 5000"))
