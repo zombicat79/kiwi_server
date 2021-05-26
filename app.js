908a0aa95534a99deea38cb7cf4816db210fe0b9
David Castejon <ombengue@hotmail.com>
2021-05-26 16:18:57 +0200

    Connect with external dictionary API - First version of predictive text

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 const dictionary = require('./javascripts/dictionary');
 const buildCombinations = require('./helpers/combinationBuilder');
 
@@ -12,7 +13,17 @@ app.use(cors({
 
 app.get('/:id', (req, res, next) => {
     const combinations = (buildCombinations(req.params.id));
-    res.send(combinations)
+    for (let i = 0; i < combinations.length; i++) {
+        axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${combinations[i]}`)
+        .then((response) => {
+            response.data.forEach((el) => {
+                if(el.meanings.length !== 0) {
+                    res.send(el.word)
+                }
+            });
+        })
+        .catch((err) => err)
+    }
 })
 
 app.listen(5000, () => console.log("Listening on port 5000"))
