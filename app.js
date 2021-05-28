ab1d35d24b7d8abad7063f96e2bfb3e7df9edf7a
David Castejon <ombengue@hotmail.com>
2021-05-28 11:58:14 +0200

    Add favorites route to get user bookmarked words

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-const dictionary = require('./javascripts/dictionary');
+
+// Main numeric input decoding algorithm.
 const buildCombinations = require('./helpers/combinationBuilder');
 
+// Favorite words storage.
+const frequentWords = {};
+
+// Server instance.
 const app = express();
 
 app.use(cors({
@@ -11,8 +16,25 @@ app.use(cors({
     origin: ['http://localhost:3000']
 }));
 
-app.get('/:id', (req, res, next) => {
-    const combinations = (buildCombinations(req.params.id));
+app.get('/favorites/:input', (req, res, next) => {
+    const { input } = req.params;
+    const formattedInput = input.toString().slice(0, input.length - 1);
+    if (frequentWords[formattedInput.length]) {
+        randomFavorite = Math.floor(Math.random() * formattedInput.length);
+        if (formattedInput.length > 1) {
+            res.send(frequentWords[formattedInput.length][randomFavorite]);
+        }
+        else {
+            res.send(frequentWords[formattedInput.length][0]);
+        }
+    }
+    else {
+        next();
+    }
+})
+
+app.get('/:input', (req, res, next) => {
+    const combinations = (buildCombinations(req.params.input));
     for (let i = 0; i < combinations.length; i++) {
         axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en_US/${combinations[i]}`)
         .then((response) => {
@@ -23,6 +45,17 @@ app.get('/:id', (req, res, next) => {
             });
         })
         .catch((err) => err)
+    }
+})
+
+app.post('/:word', (req, res, next) => {
+    const wordEntry = req.params.word;
+    if (frequentWords[wordEntry.length] === undefined) {
+        frequentWords[wordEntry.length] = [wordEntry];
+    }
+    else {
+        if (!frequentWords[wordEntry.length].includes(wordEntry))
+        frequentWords[wordEntry.length] = [...frequentWords[wordEntry.length], wordEntry];
     }
 })
 
